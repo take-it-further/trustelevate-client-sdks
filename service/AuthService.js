ea837fdc2b1f0de12e5de7a4039d7211a98e6c4b
Michal Harish <michal.harish@gmail.com>
2019-12-16 18:49:02 +0000

    babel-friendly notation for big ints

--- service/AuthService.js
+++ service/AuthService.js
@@ -3,7 +3,8 @@ const crypto = require('crypto');
 const moment = require('moment');
 
 function _signUrl(url, apiKey, apiSalt) {
-  let hash = timeBasedHash(nodeUrl.parse(url).pathname, apiSalt)
+  let u = nodeUrl.parse(url)
+  let hash = timeBasedHash(u.pathname, apiSalt)
   let signature = apiKey + ":" + hash;
   if (u.query) {
     return `${url}&signature=` + encodeURIComponent(signature);
@@ -15,16 +16,14 @@ function _signUrl(url, apiKey, apiSalt) {
 
 function timeBasedHash(arg, hexSalt, offsetInMins = 0) {
 
-  console.log("Arg: " + arg);
-
   let utcTime = moment().utc()
     .startOf('minute')
     .add(offsetInMins, 'minutes');
-  console.log("Utc time: " + utcTime.toString());
 
   let ts = utcTime.unix();
 
   let tsBuffer = Buffer.alloc(8);
+  //tsBuffer.writeBigInt64BE(BigInt(ts));
   writeBigInt64BE(tsBuffer, BigInt(ts));
 
   let buffer = Buffer.concat([
@@ -44,12 +43,11 @@ function timeBasedHash(arg, hexSalt, offsetInMins = 0) {
 // source: https://github.com/nodejs/node/blob/v12.6.0/lib/internal/buffer.js#L78-L152
 function writeBigInt64BE(buf, value, offset = 0) {
   return writeBigU_Int64BE(
-      buf, value, offset, -0x8000000000000000n, 0x7fffffffffffffffn);
+      buf, value, offset, BigInt(-0x8000000000000000), BigInt(0x7fffffffffffffff));
 }
 
 function writeBigU_Int64BE(buf, value, offset, min, max) {
-
-  let lo = Number(value & 0xffffffffn);
+  let lo = Number(value & BigInt(0xffffffff));
   buf[offset + 7] = lo;
   lo = lo >> 8;
   buf[offset + 6] = lo;
@@ -57,7 +55,7 @@ function writeBigU_Int64BE(buf, value, offset, min, max) {
   buf[offset + 5] = lo;
   lo = lo >> 8;
   buf[offset + 4] = lo;
-  let hi = Number(value >> 32n & 0xffffffffn);
+  let hi = Number(value >> BigInt(32) & BigInt(0xffffffff));
   buf[offset + 3] = hi;
   hi = hi >> 8;
   buf[offset + 2] = hi;
