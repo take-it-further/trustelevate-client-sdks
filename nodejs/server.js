bc735d7d8b2ad7834de3696a907f4b9ed4121f01
Michal Harish <michal.harish@gmail.com>
2020-01-14 15:23:56 +0000

    -

--- nodejs/server.js
+++ nodejs/server.js
@@ -6,10 +6,11 @@ app.set("views", "./");
 app.set("view engine", "pug");
 
 app.get('/', (req, res) => {
-    const apiKey = "..."
-    const apiSalt = "..."
-    const returnUrl = "..."
-    let url = veripass.signUrl("https://api.veripass.uk/v2/p/vpr/iframe?return=" + returnUrl, apiKey, apiSalt);
+    const apiKey = "trd"
+    const apiSalt = "4515000E64CEC6ACFBF6BE5D04816D63"
+    const returnUrl = "http://blackdice.com/demo?mac=xyz"
+    let url = "https://api.veripass.uk/v2/p/vpr/iframe?return=" + returnUrl
+    url = veripass.signUrl(url, apiKey, apiSalt);
     res.render('index', {signature: url});
 });
 
