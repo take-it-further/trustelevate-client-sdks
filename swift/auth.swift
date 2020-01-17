7df0c06bcb9356963e215aa1cb96e241ba586c0e
Michal Harish <michal.harish@gmail.com>
2020-01-17 14:02:29 +0000

    utf8 decoding path in swift timeBasedHash

--- swift/auth.swift
+++ swift/auth.swift
@@ -12,23 +12,10 @@ extension StringProtocol {
     }
 }
 
-extension Sequence where Iterator.Element == UInt8 {
-
-    var hex: String {
-        let format = "%02hhX"
-        return map { String(format: format, $0) }.joined()
-    }
-    func base64() -> String {
-        let bytes = self as! Array<UInt8>
-        let data = NSData(bytes: bytes, length: bytes.count)
-        return data.base64EncodedString()
-    }
-}
-
 func timeBasedHash(path: String, salt: String, offset: Int64 = 0) -> String {
     let ts = (Int64(floor(Date().timeIntervalSince1970 / 60)) + offset) * 60
     let tsendian = withUnsafeBytes(of: ts.bigEndian) { Array($0) }
-    let raw = salt.hexa + tsendian + path.bytes
+    let raw = salt.hexa + tsendian + path.data(using: .utf8)
     var digest = [UInt8](repeating: 0, count:Int(CC_SHA256_DIGEST_LENGTH))
     CC_SHA256(raw, UInt32(raw.count), &digest)
     var sha256signature = ""
